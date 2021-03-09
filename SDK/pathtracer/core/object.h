50a7a23406b8088505b3015c42daa6dc75a4b9c9
Shunji Kiuchi <lunaearth445@gmail.com>
2021-03-10 02:01:20 +0900

    Update

--- SDK/pathtracer/core/object.h
+++ SDK/pathtracer/core/object.h
@@ -5,18 +5,22 @@
 
 namespace pt {
 
-/** \note
+/** 
+ * \brief
+ * Initialize object in the device.
  * 
+ * \note 
+ * Object T must have copy constructor.
  */
 template <typename T>
-__global__ void setup_object_on_device(CUdeviceptr d_ptr) {
-    
+__global__ void setup_object_on_device(void** d_ptr, const T& obj) {
+    (*d_ptr) = new T(obj);
 }
 
 /** \brief 
  *  This class enable to use virtual function on the device.
  */ 
-class Object {
+class DeviceCallableObject {
 protected:
     virtual HOST setup_on_device(){}
     virtual HOST delete_on_device(){}
--- SDK/pathtracer/core/texture.h
+++ SDK/pathtracer/core/texture.h
@@ -27,9 +27,9 @@ inline std::ostream& operator<<(std::ostream& out, TextureType type) {
 }
 #endif
 
-class Texture : public Object {
+class Texture : public DeviceCallableObject {
     virtual HOSTDEVICE float3 eval(float2 coord, float dpdu, float dpdv) const = 0;
-    virtual HOST_ONLY TextureType type() const = 0;
+    virtual HOST TextureType type() const = 0;
 }
 
 class ConstantTexture final : public Texture {
@@ -37,53 +37,76 @@ private:
     float3 albedo;
 
 public:
-    explicit ConstantTexture(float3 a) : albedo(a) {}
+    explicit HOSTDEVICE ConstantTexture(float3 a) : albedo(a) {}
+    
+    // Copy constructor
+    explicit HOSTDEVICE ConstantTexture(const ConstantTexture& constant)
+    : albedo(constant.get_albedo()) {}
 
     HOSTDEVICE float3 eval(float2 /* coord */, float /* dpdu */, float /* dpdv */) const override {
         return albedo;
     }
 
-    HOST_ONLY TextureType type() const override { return TextureType::Constant; }
+    HOSTDEVICE float3 get_albedo() { return albedo; }
+
+    HOST TextureType type() const override { return TextureType::Constant; }
 private:
-    HOST_ONLY setup_on_device() override; 
-    HOST_ONLY delete_on_device() override;
+    HOST setup_on_device() override; 
+    HOST delete_on_device() override;
 };
 
-class CheckerTexture : public Texture {
+class CheckerTexture final : public Texture {
 private:
     float3 color1, color2;
     float scale;
 
 public:
-    CheckerTexture(float3 c1, float3 c2, float s=5)
+    HOSTDEVICE CheckerTexture(float3 c1, float3 c2, float s=5)
     : color1(c1), color2(c2), scale(s) {}
+
+    // Copy constructor
+    HOSTDEVICE CheckerTexture(const CheckerTexture& checker)
+    : color1(checker.get_color1()), color2(checker.get_color2()), scale(checker.get_scale()) {}
 
     HOSTDEVICE float3 eval(float2 coord, float /* dpdu */, float /* dpdu */) override {
         bool is_odd = sin(scale*coord.x) * sin(scale*coord.y) < 0;
         return is_odd ? color1 : color2;
     }
 
-    HOST_ONLY TextureType type() const override { return TextureType::Checker; }
+    HOST TextureType type() const override { return TextureType::Checker; }
+
+    HOSTDEVICE float3 get_color1() { return color1; }
+    HOSTDEVICE float3 get_color2() { return color2; }
+    HOSTDEVICE float scale() { return scale; }
 private: 
-    HOST_ONLY setup_on_device() override; 
-    HOST_ONLY delete_on_device() override;
+    HOST setup_on_device() override; 
+    HOST delete_on_device() override;
 };
 
-class ImageTexture {
+class ImageTexture final : public Texture {
 private:
     float3* data;
     unsigned int width, height;
 
 public:
-    explicit ImageTexture(unsigned int w, unsigned int h) : width(w), height(h) {
+    explicit HOST ImageTexture(const std::string& filename) {}
+
+    explicit HOSTDEVICE ImageTexture(unsigned int w, unsigned int h) : width(w), height(h) {
+#if !defined(__CUDACC__)
         CUDABuffer<float3> data_buffer;
         data_buffer.allocate(width*height*sizeof(float3));
         data = data_buffer.data();
+#endif
     }
+
+    explicit HOSTDEVICE ImageTexture(const ImageTexture& image)
+    : 
+
+    HOST TextureType type() const override { return TextureType::Image; }
     
 private:
-    HOST_ONLY setup_on_device() override; 
-    HOST_ONLY delete_on_device() override;
+    HOST setup_on_device() override; 
+    HOST delete_on_device() override;
 }
 
 }

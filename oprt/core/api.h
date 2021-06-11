2a3b6696afc5d63af9838abe7a4f9249bb4c10b9
Shunji Kiuchi <lunaearth445@gmail.com>
2021-06-11 14:22:48 +0900

    Added todo

--- oprt/core/api.h
+++ oprt/core/api.h
@@ -13,6 +13,47 @@ void oprtCreateMissProgram();
 void oprtCreateCallableProgram(const OptixDeviceContext& ctx, const Module& module);
 void oprtCreateHitGroupProgram();
 
+/**
+ * @todo Implement adding custom material by calling this function as like follows
+ * auto material = oprtCreateCustomMaterial<CustomMaterial>(args...)
+ */
+template <class T, class... Args> 
+std::shared_ptr<T> oprtCreateCustomMaterial(Args... args)
+{
+
+}
+
+template <class T, class... Args> 
+std::shared_ptr<T> oprtCreateCustomShape(Args... args)
+{
+
+}
+
+template <class T, class... Args> 
+std::shared_ptr<T> oprtCreateCustomTexture(Args... args)
+{
+
+}
+
+template <class... Args>
+auto oprtCreateDefinedMaterial(const std::string& name, Args... args)
+{
+    
+}
+
+template <class... Args> 
+auto oprtCreateDefinedShape(const std::string& name, Args... args) 
+{
+
+}
+
+template <class... Args> 
+auto oprtCreateDefinedTexture(const std::string& name, Args... args)
+{
+
+}
+
+
 
 
 }

1746fbfc6cb64dcaf58b416b9a5d36425248509e
Shunji Kiuchi <lunaearth445@gmail.com>
2021-08-04 14:28:46 +0900

    update

--- sutil/sutil.cpp
+++ sutil/sutil.cpp
@@ -40,9 +40,9 @@
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
 #define STB_IMAGE_IMPLEMENTATION
-#include <tinygltf/stb_image.h>
+#include "../oprt/ext/stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <tinygltf/stb_image_write.h>
+#include "../oprt/ext/stb/stb_image_write.h"
 #define TINYEXR_IMPLEMENTATION
 #include "../oprt/ext/tinyexr/tinyexr.h"
 

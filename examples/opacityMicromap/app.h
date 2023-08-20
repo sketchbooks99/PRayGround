d9570174fd3b20b9de6a5a8c190a223f6ffe1ce0
Shunji Kiuchi <lunaearth445@gmail.com>
2023-08-20 22:37:45 +0900

    [WIP] opacityMicromap

---
+++ examples/opacityMicromap/app.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <prayground/prayground.h>
+#include "params.h"
+
+class App : public BaseApp 
+{
+public:
+    void setup();
+    void update();
+    void draw();
+
+    void mousePressed(float x, float y, int button);
+    void mouseDragged(float x, float y, int button);
+    void mouseReleased(float x, float y, int button);
+    void mouseMoved(float x, float y);
+    void mouseScrolled(float xoffset, float yoffset);
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+private:
+    void initResultBufferOnDevice();
+    void handleCameraUpdate();
+
+    Context context;
+    CUstream stream;
+    Pipeline pipeline;
+
+    LaunchParams params;
+
+};
---
+++ examples/opacityMicromap/kernels.cu
@@ -0,0 +1,1 @@
+#include <prayground/prayground.h>
